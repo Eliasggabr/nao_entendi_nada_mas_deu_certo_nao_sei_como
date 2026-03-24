c9cd4e406c3ee652e7766b1372bc96ebbe3b2016
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2026-03-24 11:06:40 -0300

    commit ae
    Refactored HTML form and PHP code for data insertion.

--- index.php
+++ index.php
@@ -38,4 +38,4 @@ if ($_POST) {
     echo " Salvo no banco com sucesso!<br><br>";
 }
 
-
+//pra ligar o php com o banco de dados é so colocar o nome do banco no dbname, o usuario(root) e a senha(vazia) no PDO, e depois usar o prepare e execute pra inserir os dados no banco. E colocar o nome da tabela(que aqui é cadastro) depois de INSERT INTO, pq o coitado do php n tnha conseguido achar a tabela pq tava com o nome errado. Brutal. sobra nada pro beta. no html, criar um formulario com os campos q eu quero inserir no banco, e usar o method POST pra enviar os dados pro php. E no php, usar o $_POST pra pegar os dados do formulario e inserir no banco.
