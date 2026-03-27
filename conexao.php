7e082ff687b6155a68247622d34c22c7aa97ed2a
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2026-03-27 10:20:43 -0300

    lalalala3

---
+++ conexao.php
@@ -0,0 +1,3 @@
+<?php $conexao = new PDO("mysql:host=localhost;dbname=cadastro", "root", "");
+
+//pra ligar o php com o banco de dados é so colocar o nome do banco no dbname, o usuario(root) e a senha(vazia) no PDO
