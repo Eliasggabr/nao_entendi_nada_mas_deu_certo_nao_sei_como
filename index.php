67303d7093e835998db64a4c926861e4aa8ea882
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2026-03-27 10:18:38 -0300

    lalalala

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,7 +6,7 @@
 </head>
 <body>
 
-    <form action="" method="POST">
+    <form action="index.php" method="POST">
         <label>Nome:</label><br>
         <input type="text" name="nome" required><br><br>
         
@@ -22,20 +21,22 @@
 
 
 <?php
+include 'conexao.php';
 
-$pdo = new PDO("mysql:host=localhost;dbname=cadastro", "root", "");
-
-
-if ($_POST) {
+   if ($_POST) {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
 
     
     $sql = "INSERT INTO cadastro (nome, email) VALUES (:nome, :email)";
-    $stmt = $pdo->prepare($sql);
+    $stmt = $conexao->prepare($sql);
     $stmt->execute(['nome' => $nome, 'email' => $email]);
 
     echo " Salvo no banco com sucesso!<br><br>";
+
+    header("Location: lista.php");
+        exit(); 
 }
 
-//pra ligar o php com o banco de dados é so colocar o nome do banco no dbname, o usuario(root) e a senha(vazia) no PDO, e depois usar o prepare e execute pra inserir os dados no banco. E colocar o nome da tabela(que aqui é cadastro) depois de INSERT INTO, pq o coitado do php n tnha conseguido achar a tabela pq tava com o nome errado. Brutal. sobra nada pro beta. no html, criar um formulario com os campos q eu quero inserir no banco, e usar o method POST pra enviar os dados pro php. E no php, usar o $_POST pra pegar os dados do formulario e inserir no banco.
+
+// depois usar o prepare e execute pra inserir os dados no banco. E colocar o nome da tabela(que aqui é cadastro) depois de INSERT INTO, pq o coitado do php n tnha conseguido achar a tabela pq tava com o nome errado. Brutal. sobra nada pro beta. no html, criar um formulario com os campos q eu quero inserir no banco, e usar o method POST pra enviar os dados pro php. E no php, usar o $_POST pra pegar os dados do formulario e inserir no banco.
